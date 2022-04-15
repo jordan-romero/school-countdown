5619b58994f8cc9824394a059a8f1d30c32110f8
jordles113 <jordan.t.romero@gmail.com>
2022-04-15 14:43:04 -0500

    working on tailwind styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Timer from "./CountdownTimer/Timer";
+import HeaderContainer from "./Header/HeaderContainer";
 
 function App() {
   return (
     <div>
-      <header>
-        <h1 className="text-3xl font-bold underline">Stella's Final Days of 5th Grade</h1>
+      <header class ="flex justify-center content-center">
+        <HeaderContainer />
       </header>
         <Timer />
     </div>
---
+++ src/Header/HeaderContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HeaderContainer = () => {
+  return (
+    <div class="bg-purple-100
+                shadow
+                max-w-2xl
+                ">
+      Testing
+    </div>
+  )
+}
+
+export default HeaderContainer
